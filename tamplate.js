6d6b0c2f7bf052b4ac681435a68f785157e03e80
programmerzobayer <programmerzobayer@gmail.com>
2022-11-23 00:02:20 +0600

    Add muliline text

--- tamplate.js
+++ tamplate.js
@@ -2,3 +2,11 @@ const firstName = "Md.";
 const lastName = "Zobayer";
 const totalName= `${firstName}${lastName} Yes i am Zobayer.` ;
 console.log(totalName);
+
+// multi line 
+
+const multiLine= `Hey i am zobayer.
+I am student.
+I am 22 year old.`;
+
+console.log(multiLine);
