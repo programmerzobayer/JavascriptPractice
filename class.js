db55eb330fe3b5cae5f8483002fa7a65eaa1e949
programmerzobayer <programmerzobayer@gmail.com>
2022-11-23 13:58:55 +0600

    Upload ClassJs

---
+++ class.js
@@ -0,0 +1,11 @@
+class student{
+  constructor(sId,sName){
+  this.id = sId ;
+  this.name = sName ;
+ }
+}
+const studentOne = new student(id=1 , Name="Jubayer");
+const studentTwo = new student(id=2 , Name="Aafi");
+console.log(studentOne, studentTwo);
+console.log(studentOne.name);
+console.log(studentTwo.id);
